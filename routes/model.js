cb437f9b410495cf2b43b5a5cf8779907b7216c8
ke6ch <19749261+ke6ch@users.noreply.github.com>
2019-06-17 22:49:58 +0900

    Add eslint-plugin-jest & Update test

--- routes/model.js
+++ routes/model.js
@@ -1,10 +1,30 @@
-const { Client } = require('@elastic/elasticsearch')
-//const client = new Client({
-//  nodes: ["http://localhost:9200", "http://loalhost:9201", "http://loalhost:9202"]
-//})
+const { Client } = require("@elastic/elasticsearch");
+
+const client = new Client({
+  nodes: [
+    "http://localhost:9200",
+    "http://loalhost:9201",
+    "http://loalhost:9202"
+  ]
+});
 
 exports.show = async (req, res) => {
+  const params = {
+    index: "test_index",
+    body: {
+      query: {
+        match_all: {}
+      }
+    }
+  };
 
-  res.status(444).send("Hello World!")
+  const { body } = await client.search(params);
+  const response = body.hits.hits.map((row) => {
+    return row._source;
+  });
 
+  res.status(200).json({
+    test: response,
+    data: body.hits.total.value
+  });
 };
