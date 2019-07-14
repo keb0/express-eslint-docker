c8b186de4c4e843f588b64504ac9a2aa77193af4
ke6ch <19749261+ke6ch@users.noreply.github.com>
2019-07-15 01:33:29 +0900

    mock test

--- tests/mock.test.js
+++ tests/mock.test.js
@@ -2,24 +2,41 @@ const model = require("../models/model");
 const connect = require("../lib/connect");
 
 jest.mock("../lib/connect");
+const mockFn = jest.fn();
+connect.mockImplementation(() => {
+  return {
+    search: mockFn
+  };
+});
 
 beforeEach(() => {
   connect.mockClear();
 });
 
 test("mock test", done => {
-  connect.mockImplementation(() => {
-    return {
-      search: () => {
-        return { data: 123 }
+  mockFn.mockResolvedValue({
+    body: {
+      hits: {
+        hits: [
+          {
+            _source: {
+              data: 123
+            }
+          }
+        ],
+        total: {
+          value: 123
+        }
       }
-    };
+    }
   });
+
+  expect(connect).toHaveBeenCalledTimes(0);
 
   model.show().then(data => {
     const response = {
-      data: { data: 123 },
-      count: 1
+      data: {data: 123},
+      count: 123
     };
     expect(data).toEqual(response);
     expect(connect).toHaveBeenCalledTimes(1);
