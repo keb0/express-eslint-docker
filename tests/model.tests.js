1712bf48b906fb6f259f0898d47a6b6e54113966
k <devs079@yahoo.co.jp>
2019-07-14 22:51:42 +0900

    Add jest test

--- tests/model.tests.js
+++ tests/model.tests.js
@@ -7,13 +7,9 @@ describe("check elasticsearch", () => {
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({
-      data: 5,
-      test: [
-        { abc: "abc", tie_breaker_id: "abc" },
-        { abc: "456", tie_breaker_id: "456" },
-        { abc: "789", tie_breaker_id: "789" },
-        { abc: "123", tie_breaker_id: "123" },
-        { abc: "efg", tie_breaker_id: "efg" }
+      count: 1,
+      data: [
+        { abc: "abc", tie_breaker_id: "abc" }
       ]
     });
   });
